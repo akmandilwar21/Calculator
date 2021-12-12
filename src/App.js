1ef2d11f19af0bdbcb7b5aa89ed2438dce5ca5c2
RAMAKANT <rsy6520@gmail.com>
2021-12-12 20:14:55 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React from 'react'
+import React from "react";
 
+class Calculator extends React.Component{
+    constructor(props){
+        super(props)
+    }
 
-export default class Toggler extends React.Component{
-    
-    constructor(){
-        super();
-        this.state = {
-            htext: 'Marco',
-            btext: 'Polo'
-        };
+    express = (value) => {
+        document.getElementById('input').value += value;
     }
-    change = () => {
-        if(this.state.htext === 'Marco'){
-        this.setState({
-            htext: 'Polo',
-            btext: 'Marco'
-        })
+    calculate = () => {
+        let ex = document.getElementById('input').value;
+        document.getElementById('input').value = eval(ex);
     }
-    else{
-        this.setState({
-            htext: 'Marco',
-            btext: 'Polo'
-    })
+    clear = () => {
+        document.getElementById('input').value = "";
+    }
+    substr = () => {
+        let val = document.getElementById('input').value;
+        document.getElementById('input').value = val.substring(0, val.length-1);
+    }
+    render(){
+        return(
+        <div className="container">
+        <input className="item inp" id="input"></input>
+        <button className="item clr" onClick={() => {this.clear()}}>c</button>
+        <button className="item rem" onClick={() => {this.substr()}}>x</button>
+        <button className="item mod" onClick={() => {this.express('%')}}>%</button>
+        <button className="item divi" onClick={() => {this.express('/')}}>/</button>
+        <button className="item seven" onClick={() => {this.express(7)}}>7</button>
+        <button className="item eight" onClick={() => {this.express(8)}}>8</button>
+        <button className="item nine" onClick={() => {this.express(9)}}>9</button>
+        <button className="item multi" onClick={() => {this.express('*')}}>*</button>
+        <button className="item four" onClick={() => {this.express(4)}}>4</button>
+        <button className="item five" onClick={() => {this.express(5)}}>5</button>
+        <button className="item six" onClick={() => {this.express(6)}}>6</button>
+        <button className="item plus" onClick={() => {this.express('+')}}>+</button>
+        <button className="item one" onClick={() => {this.express(1)}}>1</button>
+        <button className="item two" onClick={() => {this.express(2)}}>2</button>
+        <button className="item three" onClick={() => {this.express(3)}}>3</button>
+        <button className="item minus" onClick={() => {this.express('-')}}>-</button>
+        <button className="item dec" onClick={() => {this.express('.')}}>.</button>
+        <button className="item zero" onClick={() => {this.express(0)}}>0</button>
+        <button className="item pow" onClick={() => {this.express('**')}}>^</button>
+        <button className="item equal" onClick={() => {this.calculate()}}>=</button>
+        </div>
+        );
     }
 }
-    render(){
-        return(<>
-        <h1 id="marco-polo">{this.state.htext}</h1>
-        <button id="marco-polo-toggler" onClick={this.change}>{this.state.btext}</button>
-        </>)
-    }
-} 
 
+export default Calculator
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Toggler from './App.js';
+import Calculator from './App.js';
+import './App.css';
+// import Flames from './code/flames.js';
+// import MyForm from './code/forms';
 
-ReactDOM.render(<Toggler />, document.getElementById('root'))
+
+ReactDOM.render(<Calculator />, document.getElementById('root'))
